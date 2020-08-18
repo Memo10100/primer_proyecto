81aaf0205f0a245f9c58790e32b2ff5c78e2b4b5
juan gonzalez <juaniiiii1025@gmail.com>
2020-08-18 16:24:26 -0300

    primer commit

---
+++ primer proyecto/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primer_proyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int miliseg = DateTime.Now.Millisecond; int dado = miliseg % 99 + 1;
+            Console.WriteLine("El numero del dado es {0}", dado);
+            Console.ReadKey();
+
+        }
+    }
+}
